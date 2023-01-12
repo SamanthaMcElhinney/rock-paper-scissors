93641d684fce64ff0407b19ab8f7680b99c8cc1f
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2023-01-12 10:17:20 -0800

    Add:constructor method, four properties for player, function for human move and function for computer move to generate random number assigned to an array

--- game.js
+++ game.js
@@ -1,6 +1,15 @@
 class Game {
     constructor(){
-        this.humanPlayer = '👩🏽‍🎤'
-        this.catPlayer = '🙀'
+        this.humanPlayer = new Player ()
+        this.catPlayer = new Player ()
+        this.wins = []
+        this.gameType = true
+        this.classicChoices = ['water', 'cat', 'cup']
+        this.difficultChoices = ['water', 'cat', 'cup', 'yarn', 'dog']
+    }
+
+    classicChoices(){
+        var playerChoice = this.humanPlayer.playerChoice
+        var computerChoice = this.catPlayer.playerChoice
     }
 }
